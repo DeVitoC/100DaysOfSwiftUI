93eb7c4ab7c76423ff248267ff359277ad5ac126
Christopher DeVito <christopher.devito@protonmail.com>
2022-02-28 15:57:30 -0500

    Added GuessTheFlag and images

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Christopher DeVito on 2/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Hello, world!")
+            Text("This is another Text view")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Christopher DeVito on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
