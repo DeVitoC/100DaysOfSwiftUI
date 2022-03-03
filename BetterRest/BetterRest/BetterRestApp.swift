541d1aa5ae4a6e87933a24470c7684cd916a52b1
Christopher DeVito <christopher.devito@protonmail.com>
2022-03-03 17:19:23 -0500

    Added BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Christopher DeVito on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
